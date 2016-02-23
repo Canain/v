3190a827f1e883e53fce6caee55668fc15d17330
Shuyang Chen <sc1085@gmail.com>
2016-02-23 15:49:12 -0500

    Added mutli argument test

--- test.js
+++ test.js
@@ -6,3 +6,5 @@ const V = require('./dist/v');
 console.log(`V.add([1, 2], [3, 4]) === ${V.add([1, 2], [3, 4])} == [4, 6]`);
 console.log(`V([1, 2]).add([3, 4]) === ${V([1, 2]).add([3, 4])} == [4, 6]`);
 console.log(`V.version === ${V.version}`);
+
+console.log(`V(1, 2).add(3, 4) === ${V(1, 2).add(3, 4)} == [4, 6]`);
