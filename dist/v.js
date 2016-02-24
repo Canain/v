72d40b48c8f545ed6c95fd13b227c2e892126637
Shuyang Chen <sc1085@gmail.com>
2016-02-23 20:58:41 -0500

    Moved V out of IIFE

--- dist/v.js
+++ dist/v.js
@@ -1,142 +1,139 @@
 'use strict';
-const V = (() => {
-    class VectorManipulateInstance {
-        constructor() {
-            this.version = '1.0.0';
-        }
-        add(a, b) {
-            if (typeof b === 'number') {
-                return a.map(v => {
-                    return v + b;
-                });
-            }
-            return a.map((v, i) => {
-                return v + b[i];
-            });
-        }
-        sub(a, b) {
-            if (typeof b === 'number') {
-                return a.map(v => {
-                    return v - b;
-                });
-            }
-            return a.map((v, i) => {
-                return v - b[i];
-            });
-        }
-        mult(a, b) {
-            if (typeof b === 'number') {
-                return a.map(v => {
-                    return v * b;
-                });
-            }
-            return a.map((v, i) => {
-                return v * b[i];
-            });
-        }
-        div(a, b) {
-            if (typeof b === 'number') {
-                return a.map(v => {
-                    return v / b;
-                });
-            }
-            return a.map((v, i) => {
-                return v / b[i];
-            });
-        }
-        floor(a) {
+class VectorManipulateInstance {
+    constructor() {
+        this.version = '1.0.0';
+    }
+    add(a, b) {
+        if (typeof b === 'number') {
             return a.map(v => {
-                return Math.floor(v);
+                return v + b;
             });
         }
-        set(a, b) {
-            b.x = a[0];
-            b.y = a[1];
-        }
-        sum(a) {
-            return a.reduce((p, v) => {
-                return p + v;
+        return a.map((v, i) => {
+            return v + b[i];
+        });
+    }
+    sub(a, b) {
+        if (typeof b === 'number') {
+            return a.map(v => {
+                return v - b;
             });
         }
-        dot(a, b) {
-            return this.sum(this.mult(a, b));
-        }
-        norm(a) {
-            return Math.sqrt(this.dot(a, a));
-        }
-        normalize(a) {
-            return this.div(a, this.norm(a));
-        }
+        return a.map((v, i) => {
+            return v - b[i];
+        });
     }
-    const V = (() => {
-        const VM = new VectorManipulateInstance();
-        const V = (function () {
-            if (typeof arguments[0] === 'number') {
-                return new Vector(Array.prototype.slice.call(arguments));
-            }
-            ;
-            return new Vector(arguments[0]);
+    mult(a, b) {
+        if (typeof b === 'number') {
+            return a.map(v => {
+                return v * b;
+            });
+        }
+        return a.map((v, i) => {
+            return v * b[i];
         });
-        Object.getOwnPropertyNames(VM).forEach(property => {
-            Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VM, property));
-        });
-        Object.getOwnPropertyNames(VectorManipulateInstance.prototype).forEach(property => {
-            if (property !== 'constructor') {
-                Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VectorManipulateInstance.prototype, property));
-            }
-        });
-        return V;
-    })();
-    const VectorProperties = {};
-    class Vector extends Array {
-        constructor() {
-            super();
-            const array = typeof arguments[0] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0];
-            Object.defineProperties(array, VectorProperties);
-            return array;
-        }
-        add() {
-            return new Vector(V.add(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-        }
-        sub() {
-            return new Vector(V.sub(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-        }
-        mult() {
-            return new Vector(V.mult(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-        }
-        div() {
-            return new Vector(V.div(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-        }
-        floor() {
-            return new Vector(V.floor(this));
-        }
-        set(b) {
-            V.set(this, b);
-            return this;
-        }
-        sum() {
-            return V.sum(this);
-            ;
-        }
-        norm() {
-            return V.norm(this);
-        }
-        normalize() {
-            return new Vector(V.normalize(this));
-        }
-        dot(b) {
-            return V.dot(this, b);
-        }
     }
-    Object.getOwnPropertyNames(Vector.prototype).forEach(property => {
+    div(a, b) {
+        if (typeof b === 'number') {
+            return a.map(v => {
+                return v / b;
+            });
+        }
+        return a.map((v, i) => {
+            return v / b[i];
+        });
+    }
+    floor(a) {
+        return a.map(v => {
+            return Math.floor(v);
+        });
+    }
+    set(a, b) {
+        b.x = a[0];
+        b.y = a[1];
+    }
+    sum(a) {
+        return a.reduce((p, v) => {
+            return p + v;
+        });
+    }
+    dot(a, b) {
+        return this.sum(this.mult(a, b));
+    }
+    norm(a) {
+        return Math.sqrt(this.dot(a, a));
+    }
+    normalize(a) {
+        return this.div(a, this.norm(a));
+    }
+}
+const V = (() => {
+    const VM = new VectorManipulateInstance();
+    const V = (function () {
+        if (typeof arguments[0] === 'number') {
+            return new Vector(Array.prototype.slice.call(arguments));
+        }
+        ;
+        return new Vector(arguments[0]);
+    });
+    Object.getOwnPropertyNames(VM).forEach(property => {
+        Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VM, property));
+    });
+    Object.getOwnPropertyNames(VectorManipulateInstance.prototype).forEach(property => {
         if (property !== 'constructor') {
-            const descriptor = Object.getOwnPropertyDescriptor(Vector.prototype, property);
-            descriptor.enumerable = false;
-            VectorProperties[property] = descriptor;
+            Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VectorManipulateInstance.prototype, property));
         }
     });
     return V;
 })();
+const VectorProperties = {};
+class Vector extends Array {
+    constructor() {
+        super();
+        const array = typeof arguments[0] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0];
+        Object.defineProperties(array, VectorProperties);
+        return array;
+    }
+    add() {
+        return new Vector(V.add(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+    }
+    sub() {
+        return new Vector(V.sub(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+    }
+    mult() {
+        return new Vector(V.mult(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+    }
+    div() {
+        return new Vector(V.div(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+    }
+    floor() {
+        return new Vector(V.floor(this));
+    }
+    set(b) {
+        V.set(this, b);
+        return this;
+    }
+    sum() {
+        return V.sum(this);
+        ;
+    }
+    norm() {
+        return V.norm(this);
+    }
+    normalize() {
+        return new Vector(V.normalize(this));
+    }
+    dot(b) {
+        return V.dot(this, b);
+    }
+}
+Object.getOwnPropertyNames(Vector.prototype).forEach(property => {
+    if (property !== 'constructor') {
+        const descriptor = Object.getOwnPropertyDescriptor(Vector.prototype, property);
+        descriptor.enumerable = false;
+        VectorProperties[property] = descriptor;
+    }
+});
 if (typeof module !== 'undefined') {
     module.exports = V;
 }
--- src/v.ts
+++ src/v.ts
@@ -1,192 +1,187 @@
 /// <reference path="../typings/main.d.ts" />
 'use strict';
 
-const V = (() => {
-	class VectorManipulateInstance {
-		
-		version: string;
-		
-		constructor() {
-			this.version = '1.0.0';
-		}
-		
-		add(a: number[], b: number | number[]) {
-			if (typeof b === 'number') {
-				return a.map(v => {
-					return v + b;
-				});
-			}
-			return a.map((v, i) => {
-				return v + (<number[]>b)[i];
-			});
-		}
-		sub(a: number[], b: number | number[]) {
-			if (typeof b === 'number') {
-				return a.map(v => {
-					return v - b;
-				});
-			}
-			return a.map((v, i) => {
-				return v - (<number[]>b)[i];
-			});
-		}
-		mult(a: number[], b: number | number[]) {
-			if (typeof b === 'number') {
-				return a.map(v => {
-					return v * b;
-				});
-			}
-			return a.map((v, i) => {
-				return v * (<number[]>b)[i];
-			});
-		}
-		div(a: number[], b: number | number[]) {
-			if (typeof b === 'number') {
-				return a.map(v => {
-					return v / b;
-				});
-			}
-			return a.map((v, i) => {
-				return v / (<number[]>b)[i];
-			});
-		}
-		floor(a: number[]) {
+class VectorManipulateInstance {
+	
+	version: string;
+	
+	constructor() {
+		this.version = '1.0.0';
+	}
+	
+	add(a: number[], b: number | number[]) {
+		if (typeof b === 'number') {
 			return a.map(v => {
-				return Math.floor(v);
+				return v + b;
 			});
 		}
-		set(a: number[], b: { x: number; y: number; }) {
-			b.x = a[0];
-			b.y = a[1];
-		}
-		sum(a: number[]) {
-			return a.reduce((p, v) => {
-				return p + v;
+		return a.map((v, i) => {
+			return v + (<number[]>b)[i];
+		});
+	}
+	sub(a: number[], b: number | number[]) {
+		if (typeof b === 'number') {
+			return a.map(v => {
+				return v - b;
 			});
 		}
-		dot(a: number[], b: number[]) {
-			return this.sum(this.mult(a, b));
-		}
-		norm(a: number[]) {
-			return Math.sqrt(this.dot(a, a));
-		}
-		normalize(a: number[]) {
-			return this.div(a, this.norm(a));
-		}
-	}
-	
-	interface VectorManipulate extends VectorManipulateInstance {
-		(a: number[]): Vector;
-		(...a: number[]): Vector;
-	}
-	
-	const V = (() => {
-		const VM = new VectorManipulateInstance();
-		
-		const V = <VectorManipulate>(function () {
-			if (typeof arguments[0] === 'number') {
-				return new Vector(Array.prototype.slice.call(arguments));
-			};
-			return new Vector(arguments[0]);
+		return a.map((v, i) => {
+			return v - (<number[]>b)[i];
 		});
-		
-		Object.getOwnPropertyNames(VM).forEach(property => {
-			Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VM, property));
-		});
-		
-		Object.getOwnPropertyNames(VectorManipulateInstance.prototype).forEach(property => {
-			if (property !== 'constructor') {
-				Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VectorManipulateInstance.prototype, property));
-			}
-		});
-		
-		return V;
-	})();
-	
-	const VectorProperties: PropertyDescriptorMap = {};
-	
-	class Vector extends Array<number> {
-		
-		constructor(a: number[]);
-		constructor(...a: number[]);
-		constructor() {
-			super();
-			
-			const array: Vector = typeof arguments[0] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0];
-			
-			Object.defineProperties(array, VectorProperties);
-			
-			return array;
-		}
-		
-		add(b: number): Vector;
-		add(b: number[]): Vector;
-		add(...b: number[]): Vector;
-		add() {
-			return new Vector(V.add(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-		}
-		
-		sub(b: number): Vector;
-		sub(b: number[]): Vector;
-		sub(...b: number[]): Vector;
-		sub() {
-			return new Vector(V.sub(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-		}
-		
-		mult(b: number): Vector;
-		mult(b: number[]): Vector;
-		mult(...b: number[]): Vector;
-		mult() {
-			return new Vector(V.mult(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-		}
-		
-		div(b: number): Vector;
-		div(b: number[]): Vector;
-		div(...b: number[]): Vector;
-		div() {
-			return new Vector(V.div(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
-		}
-		
-		floor() {
-			return new Vector(V.floor(this));
-		}
-		
-		set(b: { x: number; y: number; }) {
-			V.set(this, b);
-			
-			return this;
-		}
-		
-		sum() {
-			return V.sum(this);;
-		}
-		
-		norm() {
-			return V.norm(this);
-		}
-		
-		normalize() {
-			return new Vector(V.normalize(this));
-		}
-		
-		dot(b: number[]) {
-			return V.dot(this, b);
-		}
 	}
+	mult(a: number[], b: number | number[]) {
+		if (typeof b === 'number') {
+			return a.map(v => {
+				return v * b;
+			});
+		}
+		return a.map((v, i) => {
+			return v * (<number[]>b)[i];
+		});
+	}
+	div(a: number[], b: number | number[]) {
+		if (typeof b === 'number') {
+			return a.map(v => {
+				return v / b;
+			});
+		}
+		return a.map((v, i) => {
+			return v / (<number[]>b)[i];
+		});
+	}
+	floor(a: number[]) {
+		return a.map(v => {
+			return Math.floor(v);
+		});
+	}
+	set(a: number[], b: { x: number; y: number; }) {
+		b.x = a[0];
+		b.y = a[1];
+	}
+	sum(a: number[]) {
+		return a.reduce((p, v) => {
+			return p + v;
+		});
+	}
+	dot(a: number[], b: number[]) {
+		return this.sum(this.mult(a, b));
+	}
+	norm(a: number[]) {
+		return Math.sqrt(this.dot(a, a));
+	}
+	normalize(a: number[]) {
+		return this.div(a, this.norm(a));
+	}
+}
+
+interface VectorManipulate extends VectorManipulateInstance {
+	(a: number[]): Vector;
+	(...a: number[]): Vector;
+}
+
+const V = (() => {
+	const VM = new VectorManipulateInstance();
 	
-	Object.getOwnPropertyNames(Vector.prototype).forEach(property => {
+	const V = <VectorManipulate>(function () {
+		if (typeof arguments[0] === 'number') {
+			return new Vector(Array.prototype.slice.call(arguments));
+		};
+		return new Vector(arguments[0]);
+	});
+	
+	Object.getOwnPropertyNames(VM).forEach(property => {
+		Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VM, property));
+	});
+	
+	Object.getOwnPropertyNames(VectorManipulateInstance.prototype).forEach(property => {
 		if (property !== 'constructor') {
-			const descriptor = Object.getOwnPropertyDescriptor(Vector.prototype, property);
-			
-			descriptor.enumerable = false;
-			
-			VectorProperties[property] = descriptor;
+			Object.defineProperty(V, property, Object.getOwnPropertyDescriptor(VectorManipulateInstance.prototype, property));
 		}
 	});
 	
-	
 	return V;
 })();
+
+const VectorProperties: PropertyDescriptorMap = {};
+
+class Vector extends Array<number> {
+	
+	constructor(a: number[]);
+	constructor(...a: number[]);
+	constructor() {
+		super();
+		
+		const array: Vector = typeof arguments[0] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0];
+		
+		Object.defineProperties(array, VectorProperties);
+		
+		return array;
+	}
+	
+	add(b: number): Vector;
+	add(b: number[]): Vector;
+	add(...b: number[]): Vector;
+	add() {
+		return new Vector(V.add(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+	}
+	
+	sub(b: number): Vector;
+	sub(b: number[]): Vector;
+	sub(...b: number[]): Vector;
+	sub() {
+		return new Vector(V.sub(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+	}
+	
+	mult(b: number): Vector;
+	mult(b: number[]): Vector;
+	mult(...b: number[]): Vector;
+	mult() {
+		return new Vector(V.mult(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+	}
+	
+	div(b: number): Vector;
+	div(b: number[]): Vector;
+	div(...b: number[]): Vector;
+	div() {
+		return new Vector(V.div(this, typeof arguments[0] === 'number' && typeof arguments[1] === 'number' ? Array.prototype.slice.call(arguments) : arguments[0]));
+	}
+	
+	floor() {
+		return new Vector(V.floor(this));
+	}
+	
+	set(b: { x: number; y: number; }) {
+		V.set(this, b);
+		
+		return this;
+	}
+	
+	sum() {
+		return V.sum(this);;
+	}
+	
+	norm() {
+		return V.norm(this);
+	}
+	
+	normalize() {
+		return new Vector(V.normalize(this));
+	}
+	
+	dot(b: number[]) {
+		return V.dot(this, b);
+	}
+}
+
+Object.getOwnPropertyNames(Vector.prototype).forEach(property => {
+	if (property !== 'constructor') {
+		const descriptor = Object.getOwnPropertyDescriptor(Vector.prototype, property);
+		
+		descriptor.enumerable = false;
+		
+		VectorProperties[property] = descriptor;
+	}
+});
 
 if (typeof module !== 'undefined') {
 	module.exports = V;
